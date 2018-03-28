427c2621bfe5b0235ebea81869a27aa6988c7550
Recho <springworder@gmail.com>
2018-03-28 12:16:25 +0800

    add generate code

--- Assets/Scripts/GenerateCode.cs
+++ Assets/Scripts/GenerateCode.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using ZXing;
+using ZXing.QrCode;
 
 public class GenerateCode : MonoBehaviour {
 
+    public RawImage codeImage;
+
 	// Use this for initialization
 	void Start () {
-		
+        Generate("Hello World!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// 生成一个二维码
+    /// </summary>
+    public void Generate(string info)
+    {
+        BarcodeWriter bw = new BarcodeWriter();
+        bw.Format = BarcodeFormat.QR_CODE;
+        //需要引入QrCode命名空间
+        QrCodeEncodingOptions options = new QrCodeEncodingOptions
+        {
+            //设置optinos
+            DisableECI = true,
+            Height = 256,
+            Width= 256,
+            Margin=1,
+            CharacterSet="UTF-8",
+        };
+
+        bw.Options = options;
+
+        Color32[] code= bw.Write(info);
+        Texture2D texture2D = new Texture2D(256,256);
+        texture2D.SetPixels32(code);
+        texture2D.Apply();
+
+        codeImage.texture = texture2D;
+    }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+# 开源的ZXing二维码解决方案
+> ZXing for Net
+* [codeplex](https://archive.codeplex.com/?p=zxingnet)
+* [github](https://github.com/micjahn/ZXing.Net)
+* **Blog上的文章**
+ * [天马3798](http://www.cnblogs.com/tianma3798/p/5426869.html)
+
+## Start
+> 记录学习内容
